f2266a5c601e20a3d394a262a598ce2d9f27e28a
Ariel <baldeventertainment@gmail.com>
2016-08-21 22:57:57 -0300

    Some fixes and added Data notification

--- app/src/main/java/com/baldev/putify/helpers/VolleyHelperImplementation.java
+++ app/src/main/java/com/baldev/putify/helpers/VolleyHelperImplementation.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.util.Log;
 
 import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
 import com.android.volley.Request.Method;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -12,8 +11,8 @@ import com.android.volley.Response.ErrorListener;
 import com.android.volley.Response.Listener;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.baldev.putify.services.PutifyFCMService;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -27,78 +26,19 @@ public class VolleyHelperImplementation implements PushNotificationsManager {
 
 	public static final String URL_FCM = "https://fcm.googleapis.com/fcm/send";
 	public static final String KEY_TO = "to";
-	public static final String KEY_NOTIFICATION = "notification";
-	public static final String KEY_BODY = "body";
-	public static final String KEY_TITLE = "title";
 
 	public static PushNotificationsManager getInstance() {
 		return instance;
 	}
 
-	public static void sendTestRequest(Context context) {
-		// Instantiate the RequestQueue.
-		RequestQueue queue = Volley.newRequestQueue(context);
-		String url = "http://www.google.com";
-
-		// Request a string response from the provided URL.
-		StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-				new Response.Listener<String>() {
-					@Override
-					public void onResponse(String response) {
-						// Display the first 500 characters of the response string.
-						Log.d("Volley", "Response is: " + response.substring(0, 500));
-					}
-				}, new Response.ErrorListener() {
-			@Override
-			public void onErrorResponse(VolleyError error) {
-				Log.e("Volley", "That didn't work!");
-			}
-		});
-// Add the request to the RequestQueue.
-		queue.add(stringRequest);
-	}
-
-	public static void sendTestNotification(Context context) {
-		// Instantiate the RequestQueue.
-		RequestQueue queue = Volley.newRequestQueue(context);
-		String url = "https://fcm.googleapis.com/fcm/send";
-
-		JSONObject jsonObject = new JSONObject();
-		JSONObject notificationJson = new JSONObject();
-		try {
-			notificationJson.put("body", "This is the body");
-			notificationJson.put("title", "Title over here!");
-			jsonObject.put("notification", notificationJson);
-			jsonObject.put("to", "dwyNo9Ax40A:APA91bGCaVaFRGCA6B280R4GXKiszalb4VBCW2wjGQf3dHMixiEj4UTjYjU_Lm41JzG6CFJl9BleN43w6rYntaZihPAF79CWVIfASZ4ZG-t32rO9WG0ayCsRZnRtwz4x80gdzNGBRur-");
-		} catch (JSONException e) {
-			e.printStackTrace();
-		}
-
-		// Request a string response from the provided URL.
-		HeaderJsonObjectRequest jsonObjectRequest = new HeaderJsonObjectRequest(Method.POST, url, jsonObject,
-				new Response.Listener<JSONObject>() {
-					@Override
-					public void onResponse(JSONObject response) {
-						// Display the first 500 characters of the response string.
-						Log.d("Volley", "Response is: " + response.toString());
-					}
-				}, new Response.ErrorListener() {
-			@Override
-			public void onErrorResponse(VolleyError error) {
-				Log.e("Volley", "That didn't work!");
-			}
-		});
-
-		queue.add(jsonObjectRequest);
-	}
-
 	@Override
-	public void sendPushNotification(Context context, String to, String title, String body) {
+	public void sendPushNotification(Context context, String to, String body) {
 		// Instantiate the RequestQueue.
 		RequestQueue queue = Volley.newRequestQueue(context);
 		String url = URL_FCM;
 
-		JSONObject notificationJson = new NotificationJson(to, title, body);
+		//JSONObject notificationJson = new DataJson(to, body);
+		JSONObject notificationJson = new DataJson(to, body);
 
 		// Request a string response from the provided URL.
 		HeaderJsonObjectRequest jsonObjectRequest = new HeaderJsonObjectRequest(Method.POST, url, notificationJson,
@@ -118,6 +58,10 @@ public class VolleyHelperImplementation implements PushNotificationsManager {
 	}
 
 	public static class NotificationJson extends JSONObject {
+		private static final String KEY_NOTIFICATION = "notification";
+		private static final String KEY_BODY = "body";
+		private static final String KEY_TITLE = "title";
+
 		public NotificationJson(String to, String title, String body) {
 			JSONObject content = new JSONObject();
 			try {
@@ -131,15 +75,31 @@ public class VolleyHelperImplementation implements PushNotificationsManager {
 		}
 	}
 
+	public static class DataJson extends JSONObject {
+		private static final String KEY_DATA = "data";
+		private static final String KEY_BODY = PutifyFCMService.KEY_BODY;
+
+		public DataJson(String to, String body) {
+			JSONObject content = new JSONObject();
+			try {
+				content.put(KEY_BODY, body);
+				this.put(KEY_DATA, content);
+				this.put(KEY_TO, to);
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
 
 	public static class HeaderJsonObjectRequest extends JsonObjectRequest {
-		public HeaderJsonObjectRequest(int method, String url, JSONObject jsonRequest, Listener listener, ErrorListener errorListener) {
+		public HeaderJsonObjectRequest(int method, String url, JSONObject jsonRequest, Listener<JSONObject> listener, ErrorListener errorListener) {
 			super(method, url, jsonRequest, listener, errorListener);
 		}
 
 		@Override
-		public Map getHeaders() throws AuthFailureError {
-			Map headers = new HashMap();
+		public Map<String, String> getHeaders() throws AuthFailureError {
+			Map<String, String> headers = new HashMap<>();
 			headers.put("Content-Type", "application/json");
 			headers.put("Authorization", "key=AIzaSyBidDnnrGsfzxAUrtUKatouk6q9xXaE_8Q");
 			return headers;
